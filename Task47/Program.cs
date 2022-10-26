ea096ec003e703902213cecc617b457859394f98
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2022-10-26 18:06:02 +0300

    Task 47

---
+++ Task47/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 47: Задайте двумерный массив размером m×n,
+заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+void Main()
+{
+    int m = 3;
+    int n = 4;
+    double[,] array = GetArray(m,n);
+    PrintArray(array);
+}
+
+double[,] GetArray(int rows, int columns)
+{
+    double[,] result = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i,j] = new Random().NextDouble()*100;
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write("{0,6:F1}",inArray[i,j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+Main();
